314681169bf2e2b24e7c2fc25051268917deb432
Ekultek <thomas.perkins23@icloud.com>
2016-08-25 17:03:31 -0500

    Codecademy

---
+++ dicts_and_lists/lists_functions.py
@@ -0,0 +1,3 @@
+even_squares = [x**2 for x in range(1, 11) if (x**2) % 2 == 0]
+print even_squares
+
---
+++ dicts_and_lists/see_items_dicts.py
@@ -0,0 +1,12 @@
+my_dict = {
+    "Name": "Thomas",
+    "Age": 25,
+    "Sex": "Male"
+}
+
+print my_dict.items()
+print my_dict.keys()
+print my_dict.values()
+
+for key in my_dict:
+    print key, my_dict[key]
